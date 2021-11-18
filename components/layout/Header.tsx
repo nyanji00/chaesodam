e6b9797dfb94696e2bd3e65373ade5272e36652a
신현지 <wl77886@gmail.com>
2021-11-18 23:31:29 +0900

    Feat header (#1)
    * feat: add header
    
    * feat: main

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,9 +1,67 @@
 import styled from "@emotion/styled";
+import chaesodam from "@/public/icons/chaesodam.svg";
+import brandguide from "@/public/icons/brandguide.svg";
+import brandstory from "@/public/icons/brandstory.svg";
+import grocerybag from "@/public/icons/grocerybag.svg";
+import weeklyfruit from "@/public/icons/weeklyfruit.svg";
+
+import Image from "next/image";
 
 function Header() {
-  return <HeaderRoot></HeaderRoot>;
+  return (
+    <HeaderRoot>
+      <ImageWrapper>
+        <Image src={brandstory} />
+      </ImageWrapper>
+      <ImageWrapper>
+        <Image src={brandguide} />
+      </ImageWrapper>
+      <Image src={chaesodam} />
+      <ImageWrapper>
+        <Image src={grocerybag} />
+      </ImageWrapper>
+      <ImageWrapper>
+        <Image src={weeklyfruit} />
+      </ImageWrapper>
+    </HeaderRoot>
+  );
 }
 
-const HeaderRoot = styled.div``;
+const HeaderRoot = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  padding: 13px 200px;
+  background: #f6f0dc;
+  z-index: 100;
+  box-sizing: border-box;
+`;
+
+const ImageWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  width: fit-content;
+  cursor: pointer;
+  position: relative;
+
+  ::after {
+    position: absolute;
+    left: 50%;
+    bottom: -3px;
+    width: 0px;
+    height: 1px;
+    background: black;
+    content: "";
+    transition: all ease-in-out 0.2s;
+  }
+  :hover::after {
+    left: 0;
+    width: 100%;
+  }
+`;
 
 export default Header;
