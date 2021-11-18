1d62cc2bfea0cefac1eeb4b7d9d0cb82104c52f1
nyanji00 <wl77886@gmail.com>
2021-11-18 22:02:35 +0900

    feat: add header

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
-/** @type {import('next').NextConfig} */
+const path = require("path");
+
 module.exports = {
-  reactStrictMode: true,
-}
+  reactStrictMode: false,
+  webpack: (config, options) => {
+    config.resolve = {
+      alias: {
+        "@/components": path.join(__dirname, "components"),
+        "@/utils": path.join(__dirname, "utils"),
+        "@/public": path.join(__dirname, "public"),
+        "@/constants": path.join(__dirname, "constants"),
+      },
+      ...config.resolve,
+    };
+
+    return config;
+  },
+};
