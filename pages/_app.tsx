5d735f38cc30070caba3e76a6dcb14f5b2e1b4bd
신현지 <wl77886@gmail.com>
2021-11-22 00:50:17 +0900

    feat: slick (#4)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from "next/app";
 import Layout from "../components/layout";
 import "../styles/reset.css";
+import "slick-carousel/slick/slick.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
