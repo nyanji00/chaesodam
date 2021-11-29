c61ada66df0ae877bac2c081b81a4a9ef75c4556
nyanji00 <wl77886@gmail.com>
2021-11-30 03:15:19 +0900

    feat: copyright

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,6 +1,8 @@
 import styled from "@emotion/styled";
 import Header from "./Header";
 import { useRouter } from "next/dist/client/router";
+import Image from "next/image";
+import copyright from "@/public/icons/copyright.svg";
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -10,16 +12,24 @@ function Layout({ children }: LayoutProps) {
   const router = useRouter();
 
   return (
-    <>
+    <LayoutRoot>
       <Header />
       {children}
-      {router.route !== "/" && <Copyright></Copyright>}
-    </>
+      {router.route !== "/" && (
+        <Copyright>
+          <Image src={copyright} />
+        </Copyright>
+      )}
+    </LayoutRoot>
   );
 }
 
+const LayoutRoot = styled.div`
+  position: relative;
+`;
+
 const Copyright = styled.div`
-  position: fixed;
+  position: absolute;
   bottom: 36.18px;
   right: 200.18px;
 `;
